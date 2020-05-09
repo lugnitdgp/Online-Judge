0a3f0dbdda825056aa18baa75a149f2952be2c67
romit karmakar <romitkarmakar@gmail.com>
2020-05-09 00:04:10 +0000

    Added engine dockerfiles

---
+++ engine/java/run.sh
@@ -0,0 +1,2 @@
+docker build -t my-java-app .
+docker run -it --rm --name my-running-app my-java-app > output.txt
---
+++ engine/python3/run.sh
@@ -0,0 +1,2 @@
+sudo docker build -t my-python-app .
+sudo docker run -it --rm --name my-running-app my-python-app

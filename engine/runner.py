ce846380af773a3c7e37d9addaaa887e3976909b
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-05-12 17:11:27 +0530

    [MODIFY] script

--- engine/runner.py
+++
@@ -1,3 +0,0 @@
-from script import run_python3
-
-run_python3("temp/main.py", "temp/input.in", "temp/output.out")
--- engine/script.py
+++ engine/script.py
@@ -3,89 +3,140 @@ import subprocess
 import os
 import filecmp
 
+
+def status():
+    with open("usage.txt", "r") as f:
+        stat = f.read().split("\n")
+        return {
+            'elapsed_time': int(stat[1].split(":")[1].strip().split(" ")[0]),
+            'memory_taken': int(stat[2].split(":")[1].strip().split(" ")[0]),
+            'cpu_time': int(stat[3].split(":")[1].strip().split(" ")[0])
+        }
+
+
 def run_c(f, input_file, output_file):
     compilation = "gcc -o compiled_code " + f + " &> compile_log"
     os.system(compilation)
     if (os.stat("compile_log").st_size != 0):
-        return "Compilation Error"
+        with open("compile_log", "r+") as temp_file:
+            return {  # Compilation Error
+                "code": 1,
+                "message": temp_file.read()
+            }
     else:
         command = "sudo ./safeexec --usage usage.txt --exec compiled_code < " + input_file + " > output_of_" + input_file.split(
             ".")[0] + ".txt"
         os.system(command)
         if (filecmp.cmp(output_file,
                         "output_of_" + input_file.split(".")[0] + ".txt")):
-            return "Testcase passed"
+            return {  # Passed
+                "code": 0,
+                "status": status()
+            }
         else:
-            return "Testcase failed"
+            return {  # Failed
+                "code": 2,
+                "status": status()
+            }
+
 
 def run_cpp(f, input_file, output_file):
     compilation = "g++ -o compiled_code " + f + " &> compile_log"
     os.system(compilation)
     if (os.stat("compile_log").st_size != 0):
-        return "Compilation Error"
+        with open("compile_log", "r+") as temp_file:
+            return {  # Compilation Error
+                "code": 1,
+                "message": temp_file.read()
+            }
     else:
         command = "sudo ./safeexec --usage usage.txt --exec compiled_code < " + input_file + " > output_of_" + input_file.split(
             ".")[0] + ".txt"
         os.system(command)
         if (filecmp.cmp(output_file,
                         "output_of_" + input_file.split(".")[0] + ".txt")):
-            return "Testcase passed"
+            return {  # Passed
+                "code": 0,
+                "status": status()
+            }
         else:
-            return "Testcase failed"
+            return {  # Failed
+                "code": 2,
+                "status": status()
+            }
+
 
 def run_java(f, input_file, output_file):
     compilation = "javac " + f + " &> compile_log"
     os.system(compilation)
-    if(os.stat("compile_log").st_size != 0):
-        return "Compilation Error"
-    else:
-        command = "sudo ./safeexec --cpu 1 --mem 1000000 --nproc 20 --exec /usr/bin/java test < " + input_file + " > output_of_" + input_file.split(".")[0] + ".txt"
-        os.system(command)
-        if(filecmp.cmp(output_file, "output_of_" + input_file.split(".")[0] + ".txt")):
-            return "Testcase passed"
-        else:
-            return "Testcase failed"
-
-
-def run_python2(f, input_file, output_file):
-    compilation = "python -m py_compile " + f + " &> compile_log"
-    os.system(compilation)
     if (os.stat("compile_log").st_size != 0):
-        return "Compilation Error"
+        with open("compile_log", "r+") as temp_file:
+            return {  # Compilation Error
+                "code": 1,
+                "message": temp_file.read()
+            }
     else:
-        command = "sudo ./safeexec --usage usage.txt --exec python2 " + f + " < " + input_file + " > output_of_" + input_file.split(
+        command = "sudo ./safeexec --cpu 1 --mem 1000000 --nproc 20 --exec /usr/bin/java test < " + input_file + " > output_of_" + input_file.split(
             ".")[0] + ".txt"
         os.system(command)
         if (filecmp.cmp(output_file,
                         "output_of_" + input_file.split(".")[0] + ".txt")):
-            return "Testcase passed"
+            return {  # Passed
+                "code": 0,
+                "status": status()
+            }
         else:
-            return "Testcase failed"
+            return {  # Failed
+                "code": 2,
+                "status": status()
+            }
+
+
+def run_python2(f, input_file, output_file):
+    compilation = "python2 -m py_compile " + f + " &> compile_log"
+    os.system(compilation)
+    if (os.stat("compile_log").st_size != 0):
+        with open("compile_log", "r+") as temp_file:
+            return {  # Compilation Error
+                "code": 1,
+                "message": temp_file.read()
+            }
+    else:
+        command = "sudo ./safeexec --usage usage.txt --exec /usr/bin/python2 " + f + " < " + input_file + " > output_of_" + input_file.split(
+            ".")[0] + ".txt"
+        os.system(command)
+        if (filecmp.cmp(output_file,
+                        "output_of_" + input_file.split(".")[0] + ".txt")):
+            return {  # Passed
+                "code": 0,
+                "status": status()
+            }
+        else:
+            return {  # Failed
+                "code": 2,
+                "status": status()
+            }
 
 
 def run_python3(f, input_file, output_file):
-    compilation = "python -m py_compile " + f + " &> compile_log"
+    compilation = "python3 -m py_compile " + f + " &> compile_log"
     os.system(compilation)
     if (os.stat("compile_log").st_size != 0):
-        temp_file = open("compile_log","r+") 
-        return { # Compilation Error
-            "code": 1,
-            "message": temp_file.read()
-        }
-        temp_file.close()
+        with open("compile_log", "r+") as temp_file:
+            return {  # Compilation Error
+                "code": 1,
+                "message": temp_file.read()
+            }
     else:
-        command = "sudo ./safeexec --usage usage.txt --exec python3 " + f + " < " + input_file + " > temp/output_of_main" + ".txt"
+        command = "sudo ./safeexec --usage usage.txt --exec /usr/bin/python3 " + f + " < " + input_file + " > temp/output_of_main" + ".txt"
         os.system(command)
-        if (filecmp.cmp(output_file,
-                        "temp/output_of_main" + ".txt")):
-            return { # Passed
+        if (filecmp.cmp(output_file, "temp/output_of_main" + ".txt")):
+            return {  # Passed
                 "code": 0,
-                "message": "Testcase passed"
+                "status": status()
             }
         else:
-            temp_file = open("temp/output_of_main.txt","r+") 
-            return { # Failed
+            return {  # Failed
                 "code": 2,
-                "message": "Testcase failed \n Your output: {}".format(temp_file.read())
+                "status": status()
             }
-            temp_file.close()

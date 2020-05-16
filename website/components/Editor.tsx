891de894a5899f9f91fe598af1405acae451759d
Romit Karmakar <romitkarmakar@gmail.com>
2020-05-16 15:31:43 +0530

    Fixed env secrets

--- website/components/Editor.tsx
+++ website/components/Editor.tsx
@@ -1,35 +1,55 @@
-import { useEffect } from "react";
-import { Card, CardContent, CardHeader, CardActions, Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  Button,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { } from "@material-ui/icons";
+import {} from "@material-ui/icons";
 import * as monaco from "monaco-editor";
 
 const styles = makeStyles(() => ({
-    root: {
-        height: "50vh",
-        width: "60vw"
-    },
+  root: {
+    height: "50vh",
+    width: "60vw",
+  },
 }));
 
-export default ({ }) => {
-    const classes = styles();
+export default ({}) => {
+  const classes = styles();
+  var editor: any;
+  const [value, setValue] = useState("");
 
-    useEffect(() => {
-        monaco.editor.create(document.getElementById("container"), {
-            value: ["function x() {", '\tconsole.log("Hello world!");', "}"].join(
-                "\n"
-            ),
-            language: "javascript",
-        });
-    }, []);
-    return <Card>
-        <CardHeader title="Code editor" />
-        <CardContent>
-            <div id="container" className={classes.root}></div>
-        </CardContent>
-        <CardActions>
-            <Button color="primary">Run {"&"} Test</Button>
-            <Button color="primary" variant="outlined">Submit</Button>
-        </CardActions>
-    </Card>;
+  useEffect(() => {
+    editor = monaco.editor.create(document.getElementById("container"), {
+      value: value,
+      language: "javascript",
+    });
+  }, []);
+
+  const handleSubmit = () => {
+    setValue(editor.getValue());
+    console.log(value);
+  };
+
+  return (
+    <Card>
+      <CardHeader title="Code editor" />
+      <CardContent>
+        <div id="container" className={classes.root}></div>
+      </CardContent>
+      <CardActions>
+        <Button color="primary">Run {"&"} Test</Button>
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => handleSubmit()}
+        >
+          Submit
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };

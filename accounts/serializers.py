f37fc0379cf56eebc84ed31d04451557a257f2cb
Phantsure <samparksharma2000@gmail.com>
2020-05-20 17:20:41 +0530

    [ADD] custom login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
 from accounts.models import *
 
 
@@ -18,6 +19,27 @@ class CreateUserSerializer(serializers.ModelSerializer):
                                         email=data['email'],
                                         first_name=data['first_name'])
         return user
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    username = serializers.CharField()
+    first_name = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'password')
+
+    def create(self, data):
+        user = User.objects.create_user(username=data['username'],
+                                        email=data['email'],
+                                        first_name=data['first_name'],
+                                        password=make_password(data['password']))
+        return user
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email','password')
 
 class CoderSerializer(serializers.ModelSerializer):
     class Meta:

a12487c1be3f014ee1af9e8a5cc51c0d59337ce8
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-05-23 16:31:16 +0530

    [ADD] Code snippets

--- website/components/Editor.jsx
+++ website/components/Editor.jsx
@@ -3,6 +3,8 @@ import AceEditor from "react-ace";
 
 import "ace-builds/src-noconflict/mode-c_cpp";
 import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/snippets/c_cpp";
+import "ace-builds/src-min-noconflict/ext-language_tools";
 import { Ace } from 'ace-builds';
 
 class Editor extends Component {
@@ -19,10 +21,13 @@ class Editor extends Component {
                     theme="tomorrow"
                     onChange={this.onChange}
                     name="UNIQUE"
-                    editorProp={{ $blockScrolling: true}}
+                    editorProp={{ $blockScrolling: true }}
                     height="800px"
                     width="800px"
                     fontSize={16}
+                    enableBasicAutocompletion={true}
+                    enableLiveAutocompletion={true}
+                    enableSnippets={true}
                 />
             </div>
         )

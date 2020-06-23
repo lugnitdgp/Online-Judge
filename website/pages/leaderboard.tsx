81540c4f80c5fc7c84c6b3b79aef16671b395e48
V Rohan Rao <rohanrao.dec11@gmail.com>
2020-06-24 02:56:03 +0530

    Updated Leaderboard

--- website/pages/leaderboard.tsx
+++ website/pages/leaderboard.tsx
@@ -1,97 +1,128 @@
-import React from "react";
-import axios from "axios";
-import Layout from "../components/Layout";
-import {} from "@material-ui/icons";
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Typography,
-  Paper,
-} from "@material-ui/core";
-import { withStyles, createStyles, Theme } from "@material-ui/core/styles";
-
+import React from 'react';
+import axios from 'axios';
+import Layout from '../components/Layout';
+import {} from '@material-ui/icons';
+import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Avatar } from '@material-ui/core';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 const StyledTableCell = withStyles((theme: Theme) =>
-  createStyles({
-    head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  })
+	createStyles({
+		head: {
+			backgroundColor: theme.palette.common.black,
+			color: theme.palette.common.white,
+			fontSize: 20
+		},
+		body: {
+			fontSize: 14
+		}
+	})
 )(TableCell);
 
-interface IProps {
-  classes: any;
-}
-
 class Leaderboard extends React.Component<IProps, {}> {
-  state = {
-    gotData: false,
-    leaderBoard: [],
-  };
-  componentDidMount() {
-    // axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/leaderboard?format=json`).then(data => {
-    axios
-      .get(`https://ojapi.trennds.com/api/leaderboard?format=json`)
-      .then((data) => {
-        this.setState({ gotData: true, leaderBoard: data.data });
-      })
-      .catch(function (error) {
-        if (error.response) {
-          console.log(error);
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  }
+	state = {
+		gotData: false,
+		leaderBoard: []
+	};
 
-  render() {
-    return (
-      <Layout>
-        <TableContainer component={Paper}>
-          <Table stickyHeader aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>Rank</StyledTableCell>
-                <StyledTableCell>USER</StyledTableCell>
-                <StyledTableCell>Score</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.leaderBoard.map((user, index) => {
-                return (
-                  <tr>
-                    <TableCell>
-                      <Typography variant="body1">
-                        <strong>{index + 1}</strong>
-                      </Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1">
-                        <strong>{user.name.toUpperCase()}</strong>
-                      </Typography>
-                    </TableCell>
-                    <TableCell>
-                      <Typography variant="body1" color="secondary">
-                        <strong>{user.score}</strong>
-                      </Typography>
-                    </TableCell>
-                  </tr>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Layout>
-    );
-  }
+	componentDidMount() {
+		// axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/leaderboard?format=json`).then(data => {
+		axios
+			.get(`https://ojapi.trennds.com/api/leaderboard?format=json`)
+			.then((data) => {
+				this.setState({ gotData: true, leaderBoard: data.data });
+			})
+			.catch(function(error) {
+				if (error.response) {
+					console.log(error);
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				}
+			});
+	}
+
+	render() {
+		interface Column {
+			id: 'rank' | 'image' | 'name' | 'score';
+			label: string;
+			minWidth?: number;
+			align?: 'right';
+			format?: (value: number) => string;
+		}
+		const columns: Column[] = [
+			{ id: 'rank', align: 'center', label: 'Rank', minWidth: 100 },
+			{ id: 'image', align: 'right', minWidth: 10 },
+			{ id: 'name', align: 'left', label: 'User', minWidth: 100 },
+			{ id: 'score', align: 'center', label: 'Score', minWidth: 100 }
+		];
+
+		return (
+			<Layout>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+				<Paper
+					style={{
+						borderRadius: '25px',
+						border: '2px solid #068efd',
+						padding: '20px',
+						top: '19%',
+						left: '50%',
+						transform: 'translate(-50% , -50%)',
+						position: 'absolute'
+					}}
+					elevation={3}
+				>
+					<h1>Contest Ranks</h1>
+				</Paper>
+				<Container className="contain" maxWidth="md">
+					<Paper elevation={3}>
+						<TableContainer>
+							<Table
+								aria-label="simple table"
+								style={{
+									border: '2px solid #000000',
+									padding: '2px'
+								}}
+							>
+								<TableHead>
+									<TableRow>
+										{columns.map((column) => (
+											<StyledTableCell align={column.align} style={{ minWidth: column.minWidth }}>
+												<strong>{column.label}</strong>
+											</StyledTableCell>
+										))}
+									</TableRow>
+								</TableHead>
+
+								<TableBody>
+									{this.state.leaderBoard.map((row) => {
+										return (
+											<TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+												{columns.map((column) => {
+													const value = row[column.id];
+													return (
+														<TableCell key={column.id} align={column.align}>
+															{column.id === 'image' ? (
+																<Avatar alt={row.name} src={value} />
+															) : (
+																<strong>{value}</strong>
+															)}
+														</TableCell>
+													);
+												})}
+											</TableRow>
+										);
+									})}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Paper>
+				</Container>
+			</Layout>
+		);
+	}
 }
 
 export default Leaderboard;

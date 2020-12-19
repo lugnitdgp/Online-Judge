980e2eca7d8ac71062409b3c373fd5c47d58e4be
Romit Karmakar <romitkarmakar@gmail.com>
2020-12-19 20:28:35 +0000

    Added s3 as storage backend

--- judge/settings.py
+++ judge/settings.py
@@ -171,3 +171,9 @@ DJRICHTEXTFIELD_CONFIG = {
         'width': 700
     }
 }
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID=config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=config('AWS_STORAGE_BUCKET_NAME')

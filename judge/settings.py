e0ff591f5949724c6056d9c4dc8b872aa22fc8e8
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2021-01-02 15:09:47 +0530

    [ADD] Change file storages according to debug
    [ADD] Storages according to debug

--- judge/settings.py
+++ judge/settings.py
@@ -172,8 +172,10 @@ DJRICHTEXTFIELD_CONFIG = {
     }
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')

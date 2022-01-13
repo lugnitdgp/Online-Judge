21bffa0348c6a978eae50d4068034e02709ab2da
mayankt18 <mt.20u10405@btech.nitdgp.ac.in>
2022-01-13 13:46:53 +0530

    [REM]console logs from footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -7,11 +7,9 @@ function Footer() {
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/getsponsors`)
     .then(res => {
-      console.log(res);
       setSponsors(res.data);
     })
     .catch(err => {
-      console.log(err);
     })
   }, [])
 
